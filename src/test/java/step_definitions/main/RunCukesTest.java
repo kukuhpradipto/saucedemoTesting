64ebd4d5375de3dce1937d6789b76391f1d44232
kukuhpradipto <114077446+kukuhpradipto@users.noreply.github.com>
2024-05-07 20:34:27 +0700

    Initial commit

---
+++ src/test/java/step_definitions/main/RunCukesTest.java
@@ -0,0 +1,20 @@
+package step_definitions.main;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = {"@CreateAccount"},
+        features = {"classpath:features/"},
+        glue = { "classpath:step_definitions"},
+        plugin= {"pretty",
+                "html:target/site/cucumber-pretty",
+                "json:target/cucumber/cucumber.json"
+                }
+
+)
+
+public class RunCukesTest {
+}
